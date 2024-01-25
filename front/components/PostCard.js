c51743a37df138196508b0694497f4c1c8419a7c
haejin <luckyhaejin1126@gmail.com>
2024-01-25 16:19:55 +0900

    링크의 주소를 모두 미리 불러올 필요가 없으니 Link안에  prefecth={false} 추가

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -92,7 +92,7 @@ const PostCard = ({post}) => {
                             <div style={{float: 'right'}}>{moment(post.createdAt).format('YYYY.MM.DD')}</div>
                             <Card.Meta 
                                 avatar={(
-                                        <Link href={`/user/${post.Retweet.User.id}`}>
+                                        <Link href={`/user/${post.Retweet.User.id}`} prefecth={false}>
                                             <a><Avatar>{post.Retweet.User.nickname[0]}</Avatar></a>
                                         </Link>
                                         )}
@@ -106,7 +106,7 @@ const PostCard = ({post}) => {
                             <div style={{float: 'right'}}>{moment(post.createdAt).format('YYYY.MM.DD')}</div>
                             <Card.Meta 
                                 avatar={(
-                                        <Link href={`/user/${post.User.id}`}>
+                                        <Link href={`/user/${post.User.id}`} prefecth={false}>
                                             <a><Avatar>{post.User.nickname[0]}</Avatar></a>
                                         </Link>
                                         )}
@@ -130,7 +130,7 @@ const PostCard = ({post}) => {
                                 <Comment
                                     author={item.User.nickname}
                                     avatar={(
-                                            <Link href={`/user/${item.User.id}`}>
+                                            <Link href={`/user/${item.User.id}`} prefecth={false}>
                                                 <a><Avatar>{item.User.nickname[0]}</Avatar></a>
                                             </Link>
                                             )}
